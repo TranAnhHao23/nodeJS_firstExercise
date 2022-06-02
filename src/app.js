1277920e83b7416c72a91e116049f28cd9f16222
TranAnhHao23 <Trananhhaonuce@gmail.com>
2022-06-02 22:10:09 +0700

    add 3rd

--- src/app.js
+++ src/app.js
@@ -8,8 +8,12 @@ const { engine } = require('express-handlebars');
 const app = express();
 const port = 3000;
 
+
+
 //Http logger
 app.use(morgan('combined'));
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 //Template engine
 app.engine('hbs', engine({extname: '.hbs'}));
@@ -20,15 +24,24 @@ console.log("PATH: " + path.join(__dirname, 'resources/views'));
 
 app.get('/', (req, res) => {
     res.render('home');
-})
+});
 
 app.get('/news', (req, res) => {
     res.render('news');
+});
+
+app.get('/search', (req, res) => {
+    console.log(req.query)
+    res.render('search');
+});
+
+app.post('/search',(req, res) =>{
+    res.send("hello")
 })
 
 app.listen(port, () => {
     console.log(`Server is running on ${port} port`);
-})
+});
 
 
 
