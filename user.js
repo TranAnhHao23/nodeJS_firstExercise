952187a382e9fede7b2bfd71c5f16c6879a2f50a
TranAnhHao23 <Trananhhaonuce@gmail.com>
2022-06-02 16:34:24 +0700

    add 2nd

--- user.js
+++
@@ -1,9 +0,0 @@
-let user = [
-    {id: 1, name: "hao", age: 15},
-    {id: 2, name: "an", age: 15},
-    {id: 3, name: "nam", age: 15},
-    {id: 4, name: "na", age: 15},
-]
-module.exports = user;
-
-
